46820607b43cb0f2ed48dddde0d80cb9e8a94795
Akshay Johri <akjohri@microsoft.com>
2017-07-28 12:44:07 +0530

    Fixed MediaType

--- src/main/java/ServiceResource.java
+++ src/main/java/ServiceResource.java
@@ -3,12 +3,15 @@ import com.google.common.base.Optional;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Created by akjohri on 7/28/2017.
  */
 @Path("/service")
+@Produces(MediaType.APPLICATION_JSON)
 public class ServiceResource {
     public ServiceResource() {
 
