b118b04bf4016ffe3762376ae02618405bb751ee
Akshay Johri <akjohri@microsoft.com>
2017-07-28 16:23:38 +0530

    process cmd

--- src/main/java/ReadBC.java
+++ src/main/java/ReadBC.java
@@ -2,7 +2,10 @@ import com.thetransactioncompany.jsonrpc2.JSONRPC2ParseException;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,14 +14,45 @@ import java.util.Map;
  */
 public class ReadBC {
 
-    public static void send() {
-        String method = "liststreamitems adstream1";
-        String id = "multichainrpc";
-        Map<String, Object> params = new HashMap<String, Object>();
+    public static String send() {
+        try {
+            Process process =
+                    new ProcessBuilder(new String[] {"multichain-cli adchain1 liststreamitems adstream1"})
+                            .redirectErrorStream(true)
+                            .start();
+
+            ArrayList<String> output = new ArrayList<String>();
+            BufferedReader br = new BufferedReader(
+                    new InputStreamReader(process.getInputStream()));
+            String line = null;
+            while ( (line = br.readLine()) != null ) {
+                output.add(line);
+                System.out.println("Process:read:"+line);
+            }
+
+            //There should really be a timeout here.
+            if (0 != process.waitFor())
+                return null;
+
+            return output.toString();
+
+        } catch (Exception e) {
+            //Warning: doing this is no good in high quality applications.
+            //Instead, present appropriate error messages to the user.
+            //But it's perfectly fine for prototyping.
+
+            return null;
+        }
 
 
-        JSONRPC2Request request = new JSONRPC2Request(method, params, id);
-        SocketStuff.sendData(request.toString());
+
+//        String method = "liststreamitems adstream1";
+//        String id = "multichainrpc";
+//        Map<String, Object> params = new HashMap<String, Object>();
+//
+//
+//        JSONRPC2Request request = new JSONRPC2Request(method, params, id);
+//        SocketStuff.sendData(request.toString());
     }
 }
 
