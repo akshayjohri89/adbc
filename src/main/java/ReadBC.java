1bb57c4282816a246cdf8cf6d28aa4d39613807e
Akshay Johri <akjohri@microsoft.com>
2018-07-25 20:08:14 +0530

    Adding Ad Key

--- src/main/java/ReadBC.java
+++ src/main/java/ReadBC.java
@@ -159,8 +159,8 @@ public class ReadBC {
 
 
                     StringBuilder output = new StringBuilder();
-                    for (int i = 0; i < toReturn.length(); i+=2) {
-                        String str = toReturn.substring(i, i+2);
+                    for (int i = 0; i < data.length(); i+=2) {
+                        String str = data.substring(i, i+2);
                         output.append((char)Integer.parseInt(str, 16));
                     }
                     JSONObject returnJson = new JSONObject(output);
